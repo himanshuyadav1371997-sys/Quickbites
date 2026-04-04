ac7c3417251b355e27d32c636ae2898e2b23ceb6
himanshuyadav1371997-sys <himanshuyadav1371997@gmail.com>
2026-04-04 23:45:52 +0530

    Add signup and login functions using Firebase Auth

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -8,3 +8,37 @@ const firebaseConfig = {
   appId: "1:211934842482:web:7c772b26117e3eef283ceb",
   measurementId: "G-L27P64SRGH"
 };
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+const auth = getAuth();
+
+function signup() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      alert("Signup Success ✅");
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
+
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      alert("Login Success 🚀");
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
+
+// IMPORTANT (HTML se connect karne ke liye)
+window.login = login;
+window.signup = signup;
