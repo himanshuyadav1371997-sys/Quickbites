f1ec4485560f53f1b3f83aca7bce66267f62dd68
himanshuyadav1371997-sys <himanshuyadav1371997@gmail.com>
2026-04-04 22:36:10 +0530

    Add Firebase configuration to firebase.js

---
+++ frontend/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARAfE0MPy6SIY7OmwcYep525teFCVJy6c",
+  authDomain: "quickbites-app-23ae9.firebaseapp.com",
+  projectId: "quickbites-app-23ae9",
+  storageBucket: "quickbites-app-23ae9.firebasestorage.app",
+  messagingSenderId: "211934842482",
+  appId: "1:211934842482:web:7c772b26117e3eef283ceb",
+  measurementId: "G-L27P64SRGH"
+};
